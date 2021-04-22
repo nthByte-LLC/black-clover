cc144e81d54dc63a217a5f118231d8b94afe2881
c10coding <caleb.ja.owens@gmail.com>
2021-04-21 22:12:31 -0400

    Done with Cartographer

--- src/main/java/net/dohaw/blackclover/grimmoire/type/Compass.java
+++ src/main/java/net/dohaw/blackclover/grimmoire/type/Compass.java
@@ -4,6 +4,7 @@ import net.dohaw.blackclover.grimmoire.GrimmoireClassType;
 import net.dohaw.blackclover.grimmoire.GrimmoireType;
 import net.dohaw.blackclover.grimmoire.GrimmoireWrapper;
 import net.dohaw.blackclover.grimmoire.spell.SpellType;
+import net.dohaw.blackclover.grimmoire.spell.type.compass.Cartographer;
 import net.dohaw.blackclover.grimmoire.spell.type.compass.Direction;
 
 import java.util.Arrays;
@@ -11,6 +12,7 @@ import java.util.List;
 
 public class Compass extends GrimmoireWrapper {
 
+    public Cartographer cartographer;
     public Direction direction;
 
     public Compass() {
@@ -37,6 +39,9 @@ public class Compass extends GrimmoireWrapper {
 
         this.direction = new Direction(config);
         this.spells.put(SpellType.DIRECTION, direction);
+
+        this.cartographer = new Cartographer(config);
+        this.spells.put(SpellType.CARTOGRAPHER, cartographer);
 
     }
 
