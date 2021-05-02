d4fd052f536a76d5eabc36c3ccda4080b4bad3d3
c10coding <caleb.ja.owens@gmail.com>
2021-05-02 15:44:31 -0400

    Refactored Dome.

--- src/main/java/net/dohaw/blackclover/grimmoire/spell/type/iron/Dome.java
+++ src/main/java/net/dohaw/blackclover/grimmoire/spell/type/iron/Dome.java
@@ -7,6 +7,9 @@ import net.dohaw.blackclover.grimmoire.spell.CastSpellWrapper;
 import net.dohaw.blackclover.grimmoire.spell.DependableSpell;
 import net.dohaw.blackclover.grimmoire.spell.SpellType;
 import net.dohaw.blackclover.playerdata.PlayerData;
+import net.dohaw.blackclover.util.BlockSnapshot;
+import net.dohaw.blackclover.util.ShapeUtils;
+import net.dohaw.blackclover.util.SpellUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -18,46 +21,55 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
-public class Dome extends CastSpellWrapper implements DependableSpell, Listener {
+public class Dome extends CastSpellWrapper implements Listener {
+
+    private double duration;
+
+    private ArrayList<Material> replaceableBlocks = new ArrayList<>(Collections.singletonList(Material.GRASS));
+    private ArrayList<List<Location>> unbreakableLocs = new ArrayList<>();
 
     public Dome(GrimmoireConfig grimmoireConfig) {
         super(SpellType.DOME, grimmoireConfig);
     }
 
-    ArrayList<Material> replaceableBlocks = new ArrayList<Material>(Arrays.asList(new Material[]{
-            Material.GRASS}));
-
-    ArrayList<List<Location>> unbreakableLocs = new ArrayList<>();
-
     @Override
-    public boolean cast(Event e, PlayerData pd) throws UnexpectedPlayerData {
+    public boolean cast(Event e, PlayerData pd) {
+
         Player p = pd.getPlayer();
         
-        List<Location> sphereLocs = getSphere(p.getLocation(), 4, true);
-        HashMap<Location, Material> prevBlocks = new HashMap<>();
+        List<Location> sphereLocs = ShapeUtils.generateSphere(p.getLocation(), 4, true);
+        List<BlockSnapshot> previousBlocks = new ArrayList<>();
+
+        Location glowstoneLocation = sphereLocs.get(86);
+
         unbreakableLocs.add(sphereLocs);
         for (Location loc : sphereLocs) {
-            Block b = p.getWorld().getBlockAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+
+            Block b = loc.getBlock();
             if (b.getType() == Material.AIR || replaceableBlocks.contains(b.getType())) {
-                prevBlocks.put(b.getLocation(), b.getType());
-                if (loc.equals(sphereLocs.get(86))) b.setType(Material.GLOWSTONE);
-                else b.setType(Material.GLASS);
+
+                previousBlocks.add(BlockSnapshot.toSnapshot(b));
+                if(loc.equals(glowstoneLocation)){
+                    b.setType(Material.GLOWSTONE);
+                }else{
+                    b.setType(Material.IRON_BLOCK);
+                }
+
             }
+
         }
-        p.playSound(sphereLocs.get(86), Sound.BLOCK_GLASS_HIT, 10, 10);
+
+        SpellUtils.playSound(glowstoneLocation, Sound.BLOCK_ANVIL_PLACE);
         Bukkit.getServer().getScheduler().runTaskLater(Grimmoire.instance, () -> {
-            if (unbreakableLocs.contains(sphereLocs)) unbreakableLocs.remove(sphereLocs);
-            for (Location loc : prevBlocks.keySet()) {
-                p.getWorld().getBlockAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).setType(prevBlocks.get(loc));
+            for(BlockSnapshot snap : previousBlocks){
+                snap.apply();
             }
-            p.playSound(sphereLocs.get(86), Sound.BLOCK_GLASS_BREAK, 10, 10);
-        }, (20L) * 30);
-        return false;
+            SpellUtils.playSound(glowstoneLocation, Sound.BLOCK_ANVIL_BREAK);
+        }, (long) (20L * duration));
+
+        return true;
     }
 
     @EventHandler
@@ -73,37 +85,13 @@ public class Dome extends CastSpellWrapper implements DependableSpell, Listener 
         });
     }
 
-    public static List<Location> getSphere(Location centerBlock, int radius, boolean hollow) {
-        if (centerBlock == null) {
-            return new ArrayList<>();
-        }
-        List<Location> circleBlocks = new ArrayList<Location>();
-        int bx = centerBlock.getBlockX();
-        int by = centerBlock.getBlockY();
-        int bz = centerBlock.getBlockZ();
-        for(int x = bx - radius; x <= bx + radius; x++) {
-            for(int y = by - radius; y <= by + radius; y++) {
-                for(int z = bz - radius; z <= bz + radius; z++) {
-
-                    double distance = ((bx-x) * (bx-x) + ((bz-z) * (bz-z)) + ((by-y) * (by-y)));
-
-                    if(distance < radius * radius && !(hollow && distance < ((radius - 1) * (radius - 1)))) {
-
-                        Location l = new Location(centerBlock.getWorld(), x, y + 1, z);
-
-                        circleBlocks.add(l);
-
-                    }
-
-                }
-            }
-        }
-        return circleBlocks;
-    }
-
-    @Override
-    public void initDependableData() {}
-
     @Override
     public void prepareShutdown() {}
+
+    @Override
+    public void loadSettings() {
+        super.loadSettings();
+        this.duration = grimmoireConfig.getDoubleSetting(KEY, "Duration");
+    }
+
 }
